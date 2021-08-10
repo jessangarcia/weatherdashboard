71107950bc2101d549b5339812b577fec036728c
jessan <jessenia.garcia998@gmail.com>
2021-08-09 21:37:41 -0600

    added varibles to js page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//api key: afae4319fa1019c142d85c5c40401962
+var searchForm = document.querySelector("#search-city");
+var inputEl = document.querySelector("#city-name");
+var weatherContainer = document.querySelector("#current-city-container");
+var titleCityName = document.querySelector("#subtitle");
+var forecastTitle = document.querySelector("#forecast");
+var foreContainer = document.querySelector("#fave-container");
+
