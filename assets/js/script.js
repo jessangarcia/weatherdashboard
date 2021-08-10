285324fd13cdf1b0943edc1998a89b162603fdba
jessan <jessenia.garcia998@gmail.com>
2021-08-10 16:01:49 -0600

    date works in console log, still trying to append to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,29 @@ var titleCityName = document.querySelector("#subtitle");
 var forecastTitle = document.querySelector("#forecast");
 var foreContainer = document.querySelector("#five-container");
 
+var formSubmit = function(event) {
+    event.preventDefault();
+    var city = inputEl.value.trim();
+
+    if (city) {
+        getWeather(city);
+
+        //unshift array https://www.w3schools.com/jsref/jsref_unshift.asp
+        cities.unshift({city});
+        inputEl.value = "";
+    } else {
+        alert("Please enter a city");
+    }
+
+    saveSearch();
+}
+
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
 var getWeather = function(city) {
-    var api = "api.openweathermap.org/data/2.5/weather?q=Portland&units=imperial&appid=afae4319fa1019c142d85c5c40401962"
+    var api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=afae4319fa1019c142d85c5c40401962"
     
     fetch(api).then(function(response){
         response.json().then(function(data){
@@ -17,15 +38,20 @@ var getWeather = function(city) {
     });
 };
 
-var saveSearch = function(){
-    localStorage.setItem("cities", JSON.stringify(cities));
-}
-
 var displayWeather = function(weather, searched){
     //clear out old input
-    inputEl.textContent = searched;
     weatherContainer.textContent = "";
+    inputEl.textContent = searched;
 
+    //display city name and current date
+    var date = document.createElement("span");
+    //https://momentjs.com/docs/
+    date.textContent = " (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    inputEl.appendChild(date);
+
+    console.log(date);
 }
 
-//getWeather();
+
+searchForm.addEventListener("submit", formSubmit);
+
