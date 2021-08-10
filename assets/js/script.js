7f7b265067c530d63fc7678de83b1922f52d5c03
jessan <jessenia.garcia998@gmail.com>
2021-08-10 15:02:30 -0600

    api fetch and local storage set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
 //api key: afae4319fa1019c142d85c5c40401962
+var cities = [];
 var searchForm = document.querySelector("#search-city");
 var inputEl = document.querySelector("#city-name");
 var weatherContainer = document.querySelector("#current-city-container");
 var titleCityName = document.querySelector("#subtitle");
 var forecastTitle = document.querySelector("#forecast");
-var foreContainer = document.querySelector("#fave-container");
+var foreContainer = document.querySelector("#five-container");
 
+var getWeather = function(city) {
+    var api = "api.openweathermap.org/data/2.5/weather?q=Portland&units=imperial&appid=afae4319fa1019c142d85c5c40401962"
+    
+    fetch(api).then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city)
+        });
+    });
+};
+
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+var displayWeather = function(weather, searched){
+    //clear out old input
+    inputEl.textContent = searched;
+    weatherContainer.textContent = "";
+
+}
+
+//getWeather();
